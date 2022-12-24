7c429cdc9fd9a56f828be1ee7b7da5ead199783f
Paul Ksenofontov <mr.pavel.ksenofontov@gmail.com>
2022-12-24 11:30:52 +0300

    продвинулся дальше

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
-from bs4 import BeautifulSoup
 from pprint import pprint
 import requests
+from bs4 import BeautifulSoup
 from fake_headers import Headers
-import json
 import re
 
+new_dict = []
+
 def get_headers():
-     headers = Headers(browser='random_browser', os='random_os').generate()
-     return headers
-
-url = 'https://spb.hh.ru/search/vacancy?text=python&area=1&area=2'
-html = requests.get(url, headers=get_headers()).text
-soup = BeautifulSoup(html, features='lxml')
-data = soup.find_all('a', class_="serp-item__title")
-links = []
-for l in data:
-    link = l['href']
-    links.append(link)
+    headers = Headers(browser='random_browser', os='random_os').generate()
+    return headers
 
 
-pprint(links)
+def get_urls():
+    url = 'https://spb.hh.ru/search/vacancy?text=python&area=1&area=2'
+    response = requests.get(url, headers=get_headers())
+    soup = BeautifulSoup(response.text, features='lxml')
+    data = soup.find_all('a', class_="serp-item__title")
+    for l in data:
+        link = l['href']
+        yield link
+
+
+def array():
+    for link in get_urls():
+        try:
+            response = requests.get(link, headers=get_headers())
+            soup = BeautifulSoup(response.text, 'lxml')
+            data = soup.find('div', class_='main-content')
+            researh = r'.*((D|d)jango).*((F|f)lask).*'
+            if re.match(researh, data.find('div',class_='vacancy-title').text):
+                new_dict.append({
+                    'url':link,
+                    'salary':data.find('span', class_='bloko-header-section-2 bloko-header-section-2_lite').text,
+                    'name':data.find('div',class_='vacancy-title').text,
+                    'city':data.find('div', class_='vacancy-section').text
+                })
+            pprint(new_dict)
+        except AttributeError:
+            continue
 
 
 
-
-
-
-
-
-
-
-
+array()
